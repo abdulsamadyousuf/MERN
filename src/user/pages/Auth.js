c55cd284a1f936027cc9a92858b487b9efdc31f3
Abdul Samad <abdul.samad@10pearls.com>
2024-02-07 19:13:10 +0500

    Fixed bug

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -93,7 +93,7 @@ const Auth = (props) => {
         formData.append("password", formState.inputs.password.value);
         formData.append("image", formState.inputs.image.value);
         const responseData = await sendRequest(
-          `${$process.env.REACT_APP_BACKEND_URL}/users/signup`,
+          `${process.env.REACT_APP_BACKEND_URL}/users/signup`,
           "POST",
           formData
         );
